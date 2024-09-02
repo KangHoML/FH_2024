08b22c6a575b7cc6e1a0c5c2edfdbc9a64a35b9c
KangHoML <kanghokhkh1@gmail.com>
2024-09-02 20:03:42 +0900

    subtask3 - need to adjust hyper-parameter

--- subtask3/run.sh
+++ subtask3/run.sh
@@ -6,18 +6,21 @@ DATA_PATH="/home/ho/workspace/datasets/FH_2024/subtask3"
 # 현재 날짜와 시간
 TIMESTAMP=$(date +"%Y%m%d")
 
+# SEED
+SEED=42
+
 # 학습 및 평가 함수 정의
 run_task() {
     local task_num=$1
     
     # 학습 실행
     echo "Training for Task $task_num"
-    sh run_train.sh --seed 42 --train_diag_path "${DATA_PATH}/task${task_num}.ddata.wst.txt"
+    sh run_train.sh --seed $SEED --train_diag_path "${DATA_PATH}/task${task_num}.ddata.wst.txt"
     
     # 평가 실행
     for i in $(seq 1 $task_num); do
         echo "Evaluating on Task $i"
-        sh run_eval.sh --seed 42 --val_diag_path "${DATA_PATH}/cl_eval_task${i}.wst.dev"
+        sh run_eval.sh --seed $SEED --val_diag_path "${DATA_PATH}/cl_eval_task${i}.wst.dev" --ckpt "model_${SEED}_best.pt"
     done
     
     echo "Task $task_num completed"
